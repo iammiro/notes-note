3b85712ef5ea94ca8368bfeb2b6888f7ef613e9d
iammiro <mir.kolomiets@gmail.com>
2017-10-05 18:40:35 +0300

    add yargs

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('Starting App.');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log('Process: ', process.argv);
+console.log('Yargs: ', argv);
+
+if (command === 'add') {
+    console.log('Adding new note')
+} else if (command === 'list') {
+    console.log('Listing all notes')
+} else if (command === 'read') {
+    console.log('Fetching notes')
+} else if (command === 'remove') {
+    console.log('Delete note')
+} else {
+    console.log('Command not recognized')
+}
